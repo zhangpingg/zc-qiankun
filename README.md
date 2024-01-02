2d5f662d3059819b7e3fefd111b254c64236042b
zhangping <2425238116@qq.com>
2024-01-02 10:37:28 +0800

    fix: 优化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # zc-qiankun
-微前端：vite, qiankun
+
+微前端：
+主应用-vue3
+子应用一-vue2
+子应用二-vue3
--- base-vue/src/main.js
+++ base-vue/src/main.js
@@ -1,32 +1,32 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import {start, registerMicroApps} from 'qiankun';
-import router from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { start, registerMicroApps } from "qiankun";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
 
 const apps = [
-  {
-    name: 'sub-vue2',
-    entry: '//127.0.0.1:8080',
-    activeRule: '/sub-vue2',
-    container: '#subVue2Id',
-    props: {
-      data: '基座的数据',
-    }
-  },
-  {
-    name: 'sub-vue3',
-    entry: '//127.0.0.1:3002',
-    activeRule: '/sub-vue3',
-    container: '#subVue3Id',
-    props: {
-      data: '基座的数据',
-    }
-  },
-]
+    {
+        name: "sub-vue2",
+        entry: "//localhost:8080",
+        activeRule: "/sub-vue2",
+        container: "#subVue2Id",
+        props: {
+            data: "基座的数据",
+        },
+    },
+    {
+        name: "sub-vue3",
+        entry: "//localhost:3002",
+        activeRule: "/sub-vue3",
+        container: "#subVue3Id",
+        props: {
+            data: "基座的数据",
+        },
+    },
+];
 
-registerMicroApps(apps, {})
+registerMicroApps(apps, {});
 
-start()
+start();
