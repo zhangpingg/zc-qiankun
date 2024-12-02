f8340cd932cc9853cca8ee6220978a91033b37ac
zhangping <2425238116@qq.com>
2024-12-02 18:03:43 +0800

    feat: 优化基座的配置文件

--- base-vue/config/subEnv.js
+++ base-vue/config/subEnv.js
@@ -6,17 +6,17 @@ const getSubApplyUrl = () => {
             urlObj.subVue2Url = 'http://localhost:8081';
             urlObj.subVue3Url = 'http://localhost:8082';
             break;
-        case 'aaa.com': // 测试
+        case 'dtdealer.zhilun-k8s.com': // 测试
             urlObj.subVue2Url = 'http://12.123.123.80:8080';
             urlObj.subVue3Url = 'http://12.123.123.80:8081';
             break;
-        case 'bbb.com': // 准生产
-            urlObj.subVue2Url = 'http://12.123.123.80:8080';
-            urlObj.subVue3Url = 'http://12.123.123.80:8081';
+        case '47.115.203.84:123': // 准生产（只能通过ip访问：http://47.115.203.84:123）
+            urlObj.subVue2Url = 'http://47.115.203.84:111';
+            urlObj.subVue3Url = 'http://47.115.203.84:222';
             break;
-        case 'dccc.com': // 生产
-            urlObj.subVue2Url = 'https://vue2.z2.xxx';
-            urlObj.subVue3Url = 'https://vue3.z2.yyy';
+        case 'dtdealer.zcrubber.com': // 生产
+            urlObj.subVue2Url = 'https://vue2.z2.ltd';
+            urlObj.subVue3Url = 'https://vue3.z2.ltd';
             break;
     }
     return urlObj;
